ab5ca84d22982a36ba859dc0d1e6530746af3664
alltonp <paul.allton@theorem.technology>
2015-07-15 15:49:23 +0100

    de-privatise

--- src/main/scala/im/mange/backdoor/server/BackdoorMessageHandler.scala
+++ src/main/scala/im/mange/backdoor/server/BackdoorMessageHandler.scala
@@ -7,11 +7,11 @@ import net.liftweb.http.{PlainTextResponse, PostRequest, Req, _}
 import net.liftweb.json._
 
 trait BackdoorMessageHandler {
-   def handle(message: Any): Box[LiftResponse]
- }
+  def handle(message: Any): Box[LiftResponse]
+}
 
 object BackdoorMessageHandler {
-   def ok = respond("OK")
-   def fail(message: String) = respond(s"FAIL: $message")
-   private def respond(content: String) = Full(PlainTextResponse(content))
- }
+  def ok = respond("OK")
+  def fail(message: String) = respond(s"FAIL: $message")
+  def respond(content: String) = Full(PlainTextResponse(content))
+}
