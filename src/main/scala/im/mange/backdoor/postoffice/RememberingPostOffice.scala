54cd5e0395b1b8ea5433be2e0f322adbfd18833b
alltonp <paul.allton@theorem.technology>
2015-07-15 17:34:44 +0100

    make letters better and more tidier

--- src/main/scala/im/mange/backdoor/postoffice/RememberingPostOffice.scala
+++ src/main/scala/im/mange/backdoor/postoffice/RememberingPostOffice.scala
@@ -3,36 +3,25 @@ package im.mange.backdoor.postoffice
 import im.mange.backdoor.Resettable
 import org.joda.time.DateTime
 
-case class Entry(when: DateTime, what: Any)
+case class Letter(who: String, when: DateTime, what: Any)
 
 object RememberingPostOffice extends PostOffice with Resettable {
-  private var lettersByAddresse: scala.collection.concurrent.TrieMap[String, Seq[Entry]] =
+  private var lettersByAddresse: scala.collection.concurrent.TrieMap[String, Seq[Letter]] =
     new scala.collection.concurrent.TrieMap()
 
   override def post(message: Any, to: String): Unit = {
     //TODO: make this a debug flag
-    val current = lettersByAddresse.getOrElse(to, Seq.empty[Entry])
-    lettersByAddresse.update(to, current :+ Entry(new DateTime(), message))
-//    println(s"$to got $message - now has: ${lettersByAddresse(to).size}")
+    val current = lettersByAddresse.getOrElse(to, Seq.empty[Letter])
+    lettersByAddresse.update(to, current :+ Letter(to, new DateTime(), message))
   }
 
   def reset() {
     lettersByAddresse = new scala.collection.concurrent.TrieMap()
   }
 
-  //TODO: validateReceived? or something
-  def validate(to: String, expected: Any) = {
-    //TODO: obviously we can make this tighter ..
-    val all = lettersFor(to)
-//    println(s"all: $all")
-    val r = all.contains(expected)
-//    println(s"$expected for $to in ${all.mkString}? $r - ${lettersByAddresse.keys} - ${lettersByAddresse.contains(to)}")
-    r
-  }
-
+  def validate(to: String, expected: Any) = lettersFor(to).contains(expected)
   def lettersFor(me: String) = lettersByAddresse(me).map(_.what)
-
-  def state = lettersByAddresse
+  def allLetters = lettersByAddresse.values.flatten
 }
 
 trait PostOffice {
